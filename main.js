bfb7085c1aa3df31ba160cc1d2f9a5480e6179dc
AlexShamry <162981732+AlexShamry@users.noreply.github.com>
2024-03-28 19:02:14 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+Age = 0;
+let ageLabel = document.querySelector("label");
+function SetAge() {
+    Age = Number(window.prompt("what is you age?"));
+    ageLabel.textContent = `Your age is ${Age}`;
+    localStorage.setItem("Age", Age)
+}
+if(!localStorage.getItem("Age")){
+    SetAge();
+} else {
+    Age = Number(localStorage.getItem("Age"));
+    ageLabel.textContent = `Your age is ${Age}`;
+}
+document.getElementById("up").onclick = () => {
+    ++Age;
+    ageLabel.textContent = `Your age is ${Age}`;
+    localStorage.setItem("Age", Age);
+}
+document.getElementById("down").onclick = () => {
+    if (Number(Age) === 0){
+        ageLabel.textContent = `You\'re dead!`;
+    }
+    else {
+        --Age;
+        ageLabel.textContent = `Your age is ${Age}`;
+        localStorage.setItem("Age", Age);
+    }
+}
